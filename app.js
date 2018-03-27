f8594d8c86b9d9105f4b838fef63caf50abd38ce
iposton <ianposton@sbcglobal.net>
2018-03-26 23:13:03 -0700

    heroku

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 var express = require('express');
 var socket = require('socket.io');
+const http = require('http');
+const path = require('path');
 
 var app = express();
+
+
+app.use(express.static(path.join(__dirname, 'build')));
 
 const port = process.env.PORT || '8080';
 app.set('port', port);
